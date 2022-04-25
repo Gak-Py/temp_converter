c7440617d62febd8251516b013e1f9b8cb6822af
jipdesign-jp <jipdesigninfo@gmail.com>
2022-04-25 17:59:24 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+window = Tk()
+window.title("℃ to℉ converter")
+
+my_label = Label(text="℃", font=("Futura", 15, "bold"))
+my_label.grid(column=2, row=0,padx=10, pady=10)
+
+to_f = Label(text="is equal to", font=("Futura", 20, "bold"))
+to_f.grid(column=0, row=1,padx=10, pady=10)
+
+
+input_f = Label(text="0", font=("Futura", 30, "bold"))
+input_f.grid(column=1, row=1,padx=10, pady=10)
+
+num_c = Label(text="℉", font=("Futura", 20, "bold"))
+num_c.grid(column=2, row=1,padx=10, pady=10)
+
+#input
+input_c = Entry(width=10)
+input_c.grid(row=0, column=1,padx=10, pady=10)
+
+#button
+def encode():
+    temp_c = int(input_c.get())
+    temp_f = temp_c * 1.8 + 32
+    input_f.config(text=temp_f)
+
+button = Button(text="℉ is...", command=encode)
+button.grid(column=1, row=2,padx=10, pady=10)
+#
+# button2 = Button(text="don't click!", command=button_pressed)
+# button2.grid(column=1, row=1)
+
+#
+# def calculate(n, **kwargs):
+#     print(kwargs)
+#     n += kwargs["add"]
+#     n *= kwargs["multiple"]
+#     n -= kwargs["subtraction"]
+#     n /= kwargs["division"]
+#     return n
+
+window.mainloop()
+
